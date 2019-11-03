48bc6511b39f18cfd5473b41b7a6398ea7ab206b
Andy Philpotts <andy@failfree.net>
2019-11-03 18:27:55 -0500

    Refactored into two methods

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::env;
+use std::io;
 use std::path::PathBuf;
 use std::time::Instant;
 use walkdir::WalkDir;
+
+struct ScanSummary {
+    file_count: i32,
+    skipped_files: i32,
+    bytes_read: usize,
+    total: i64,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,21 +22,36 @@ fn main() {
 
     if !dir.is_dir() {
         println!("{:?} is not a folder I can scan!", dir);
+        return;
     }
 
     println!("Scanning folder {:?}...", dir);
 
+    let timer = Instant::now();
+
+    let scan_result = scan(&dir);
+
+    let elapsed = timer.elapsed().as_millis();
+
+    match scan_result {
+        Ok(s) => println!(
+            "Finished in {}ms, found {} files, skipped {}, bytes read {}, checksum {}",
+            elapsed, s.file_count, s.skipped_files, s.bytes_read, s.total
+        ),
+        Err(e) => println!("Scan failed! {}", e),
+    }
+}
+
+fn scan(dir: &PathBuf) -> io::Result<ScanSummary> {
     // Some metrics
     let mut file_count = 0;
     let mut skipped_files = 0;
     let mut bytes_read: usize = 0;
 
-    // we are computing the sum of all bytes, in all files
-    let mut total: u64 = 0;
+    // we are computing a checksum over all files
+    let mut total: i64 = 0;
 
-    let timer = Instant::now();
-
-    for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new(dir).into_iter().filter_map(Result::ok) {
         if entry.file_type().is_file() {
             file_count += 1;
 
@@ -36,9 +59,8 @@ fn main() {
                 Ok(bytes) => {
                     bytes_read += bytes.len();
 
-                    // Why is this loop so slow?
                     for b in bytes {
-                        total += b as u64
+                        total += b as i64
                     }
                 }
                 Err(_) => skipped_files += 1,
@@ -46,10 +68,10 @@ fn main() {
         }
     }
 
-    let elapsed = timer.elapsed().as_millis();
-
-    println!(
-        "Finished in {}ms, found {} files, skipped {}, bytes read {}, checksum {}",
-        elapsed, file_count, skipped_files, bytes_read, total
-    );
+    Ok(ScanSummary {
+        file_count,
+        skipped_files,
+        bytes_read,
+        total,
+    })
 }
